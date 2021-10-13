2076fcf5d63caa53b09ffa71824c341f9008cdab
wonnexd <85105048+wonnexd@users.noreply.github.com>
2021-10-13 10:47:59 +0200

    fsd

--- blog/forms.py
+++ blog/forms.py
@@ -2,19 +2,16 @@ from django import forms
 from django.core.mail import message
 
 class ContactForm(forms.Form):
-
 	subject = forms.CharField(widget=forms.TextInput(
 		attrs={
 			'class': 'form-control' 
 		}
 	))
-
-    from_email = forms.CharField(widget=forms.EmailInput(
+	from_email = forms.CharField(widget=forms.EmailInput(
 		attrs={
 			'class': 'form-control' 
 		}
 	))
-
 	message = forms.CharField(widget=forms.Textarea(
 		attrs={
 			'class': 'form-control' 
