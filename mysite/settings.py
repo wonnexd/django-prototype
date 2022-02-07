9c4b622616023d40f8b74843e248ec4be888d481
Timow <85105048+wonnexd@users.noreply.github.com>
2022-02-07 09:34:28 +0100

    django secret key hidden

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "vf5hh4x@6+&8i7*h5ac*heju))x-nhp_@0ypb(+p(8q_x--fnq"
+# hide django secret key
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
